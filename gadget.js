3d9c0fc8e74d8f8f0fce7e8f4cd0c144eeff2764
tako-netakun <netakun107@gmail.com>
2022-09-26 13:05:48 +0900

    a

--- gadget.js
+++ gadget.js
@@ -2,5 +2,18 @@ const menu = document.getElementById("menu");
 const menuContent = document.getElementsByClassName("menu-content");
 menu.addEventListener("mouseover", (e) => {
     e.preventDefault();
-    [...menuContent].map(x => x.style.display = "block")
+    [...menuContent].map(x => {
+        x.classList.add("animated__fadeIn");
+        x.classList.remove("animated__fadeOut");
+    
+        x.style.display = "block";
+    });
 });
+menu.addEventListener("mouseleave", (e) => {
+    e.preventDefault();
+    [...menuContent].map(x => {
+        x.classList.add("animated__fadeOut");
+        x.classList.remove("animated__fadeIn")
+        x.style.display = "none"
+    });
+})
